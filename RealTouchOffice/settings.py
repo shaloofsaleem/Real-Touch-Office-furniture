888efb7324a4c83666bc3ef96597da2031baaafb
shaloofsaleem <shalushaz01@gmail.com>
2024-01-15 23:11:43 -0800

    database

--- RealTouchOffice/settings.py
+++ RealTouchOffice/settings.py
@@ -58,6 +58,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
 ]
 
 ROOT_URLCONF = 'RealTouchOffice.urls'
